636ac20c2af5e07177f2fb2b3ccede7239027d39
DyuvenzhiMaxim <Tyuiu.DyuvenzhiMI@outlook.com>
2025-11-12 02:49:08 +0500

    Task6

---
+++ Tyuiu.DyuvenzhiMI.Sprint4.Task6.V20.Test/DataSeviceTest.cs
@@ -0,0 +1,19 @@
+﻿
+using Tyuiu.DyuvenzhiMI.Sprint4.Task6.V20.Lib;
+
+namespace Tyuiu.DyuvenzhiMI.Sprint4.Task6.V20.Test
+{
+    [TestClass]
+    public sealed class DataSeviceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            string[] mas = new string[] { "Математика", "Физика", "Химия", "Биология", "География", "История", "Литература" };
+            string[] res = ds.Calculate(mas);
+            string[] wait = new string[] { "Физика", "Химия", "Биология", "География", "История" };
+            CollectionAssert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.DyuvenzhiMI.Sprint4.Task6.V20/Program.cs
@@ -0,0 +1,41 @@
+﻿
+using Tyuiu.DyuvenzhiMI.Sprint4.Task6.V20.Lib;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Title = "Спринт #2 | Выполнил: Дювенжи М. И. | ПИНТб-25-1";
+        //Длина строки 75 символов
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #4                                                               *");
+        Console.WriteLine("* Тема: Базовые навыки работы в C#                                        *");
+        Console.WriteLine("* Задание #6                                                              *");
+        Console.WriteLine("* Вариант #20                                                             *");
+        Console.WriteLine("* Выполнил: Дювенжи Максим Иванович | ПИНТб-25-1                          *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Написать программу                                                      *");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+
+        DataService ds = new DataService();
+        string[] mas = new string[] { "Математика", "Физика", "Химия", "Биология", "География", "История", "Литература" };
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+        string[] res = ds.Calculate(mas);
+        
+        for (int i = 0; i < res.Length; i++)
+        {
+            Console.Write(res[i] + " ");
+        }
+
+        Console.ReadLine();
+
+    }
+}
